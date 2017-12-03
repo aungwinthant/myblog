142cf0a528499a1223b7bec9d6799ceb1f26d253
aungwinthant <agwinthant@gmail.com>
2017-12-03 22:46:19 +0630

    Remove Virtual Environment

--- DjangoENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/po/Documents/django_projects/Part-5/learning_user_auth/DjangoENV/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
